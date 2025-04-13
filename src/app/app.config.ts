d7968a6cf2cc916ebdf8e325e04e29c00aa14d13
Ana-Gabs <2022371069@uteq.edu.mx>
2025-04-13 16:17:03 -0600

    "app configs pets"

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,37 @@
 import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideToastr } from 'ngx-toastr';
+import { provideHttpClient } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { routes } from './app.routes';
+
+// Reducers
+import { petReducer } from './store/Pet.Reducer';
+
+// Effects
+import { PetEffects } from './store/Pet.Effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideStore(), provideEffects(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+    provideToastr(),
+    provideStore({
+      pet: petReducer,
+
+
+    }),
+    provideEffects([
+      PetEffects,
+
+
+    ]),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+  ]
 };
