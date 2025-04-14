7760e46fad60f500c0491858e096df49f9191a76
Ana-Gabs <2022371069@uteq.edu.mx>
2025-04-14 08:14:40 -0600

    "route user"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 // ./app.routes.ts
 import { Routes } from '@angular/router';
 import { PetComponent } from './components/pet/pet.component';
+import { UserComponent } from './components/user/user.component';
 
 export const routes: Routes = [
-  //{ path: 'mascotas', component: PetComponent }
-  { path: '', component: PetComponent }
+  { path: '', component: UserComponent },
+  { path: 'mascotas', component: PetComponent }
 
 ];
